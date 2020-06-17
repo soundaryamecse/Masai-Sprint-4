16b094b106a465b576407cb2cc9b93cde6630b18
soundaryamecse <soundaryamecse@gmail.com>
2020-06-17 16:06:50 +0530

    script Added

---
+++ script.js
@@ -0,0 +1,10 @@
+var register_user=[]
+var admin=[{'admin':'admin','pasword':'12345'}]
+
+function get_details()
+{
+
+}
+
+var sign_in=document.getElementById('register')
+sign_in.addEventListener('click',get_details)
